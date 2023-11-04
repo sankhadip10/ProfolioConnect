4d2344285259de1238628974702fc2fa33b3d195
Sankhadip Das <sankhadip10.das@gmail.com>
2023-11-04 23:50:52 +0530

    fnfnf

--- Practice/pyth.py
+++ Practice/pyth.py
@@ -10,6 +10,11 @@ def update_diff_array(diff, i, j, value):
     if j+1 < len(diff):
         diff[j+1] -= value
 
+def apply_queries(diff, queries):
+    # Apply each query to the difference array
+    for i, j, value in queries:
+        update_diff_array(diff, i, j, value)
+
 def reconstruct_from_diff(diff):
     arr = [0] * (len(diff) - 1)
     arr[0] = diff[0]
@@ -17,10 +22,23 @@ def reconstruct_from_diff(diff):
         arr[i] = arr[i-1] + diff[i]
     return arr
 
-# Sample array and queries
-arr = [5, 10, 15]
+# Original array
+arr = [5, 10, 15, 20, 25]
+
+# Queries (i, j, value) - assume 0-indexed
+queries = [
+    (0, 2, 5),  # Add 5 to range [0, 2]
+    (1, 3, 10), # Add 10 to range [1, 3]
+    (3, 4, -5)  # Subtract 5 from range [3, 4]
+]
+
+# Initialize difference array
 diff = initialize_diff_array(arr)
-print("++++++",diff)
-# update_diff_array(diff, 0, 1, 5)
-# arr_updated = reconstruct_from_diff(diff)
-# print(arr_updated)  # [10, 15, 15]
+
+# Apply the queries
+apply_queries(diff, queries)
+
+# Reconstruct the final array
+arr_updated = reconstruct_from_diff(diff)
+
+print(arr_updated)  # This will print the updated array after applying all queries
--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -1,40 +1,33 @@
 <template>
   <div class="flex min-h-screen bg-gray-100">
-    <div class="flex-1 flex justify-center items-center bg-cover" style="background-image: url('D:\ProfolioConnect\frontend\src\assets\hero.png');">
+    <!-- Bind the backgroundImage style to a computed property or data property -->
+    <div class="flex-1 flex justify-center items-center bg-cover" :style="backgroundImageStyle">
       <!-- You can add more images or text here -->
     </div>
-    <div class="flex-1 flex justify-center items-center">
-      <div class="w-full max-w-xs">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-              Email
-            </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email">
-          </div>
-          <div class="mb-6">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-              Password
-            </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-          </div>
-          <div class="flex items-center justify-between">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-              Sign In
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <!-- The rest of your template -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'LoginComponent',
-  // You can add your JavaScript here
+  data() {
+    return {
+      // Assuming your image is located at src/assets/hero.png
+      heroImage: require('@/assets/hero.png')
+    };
+  },
+  computed: {
+    backgroundImageStyle() {
+      return {
+        backgroundImage: `url(${this.heroImage})`
+      };
+    }
+  }
 };
 </script>
+
+
 
 <style scoped>
 /* You can add additional styles here */
